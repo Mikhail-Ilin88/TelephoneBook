5d4d8017ff52eb3d8bbc8e13d1281c83b9420c99
DESKTOP-LT96LSU\Майкл <a@a.com>
2022-07-21 21:49:01 +0300

    первый коммит.

---
+++ TelephoneBook/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace TelephoneBook_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.l = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbFind = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvFind = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 41);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(330, 203);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(62, 301);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(231, 23);
+            this.tbNumber.TabIndex = 2;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(62, 361);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(231, 23);
+            this.tbName.TabIndex = 3;
+            // 
+            // l
+            // 
+            this.l.AutoSize = true;
+            this.l.Location = new System.Drawing.Point(62, 283);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(45, 15);
+            this.l.TabIndex = 4;
+            this.l.Text = "Номер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 343);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Имя";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbFind
+            // 
+            this.tbFind.Location = new System.Drawing.Point(458, 76);
+            this.tbFind.Name = "tbFind";
+            this.tbFind.Size = new System.Drawing.Size(330, 23);
+            this.tbFind.TabIndex = 7;
+            this.tbFind.TextChanged += new System.EventHandler(this.tbFind_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(458, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Имя или номер:";
+            // 
+            // dgvFind
+            // 
+            this.dgvFind.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvFind.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvFind.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFind.Location = new System.Drawing.Point(458, 120);
+            this.dgvFind.MultiSelect = false;
+            this.dgvFind.Name = "dgvFind";
+            this.dgvFind.ReadOnly = true;
+            this.dgvFind.RowTemplate.Height = 25;
+            this.dgvFind.Size = new System.Drawing.Size(330, 139);
+            this.dgvFind.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(458, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Результат:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Все контакты:";
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(639, 274);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(149, 50);
+            this.btDelete.TabIndex = 9;
+            this.btDelete.Text = "Удалить выбранные контакты";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFind);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.l);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.dgvFind);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFind)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label l;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbFind;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvFind;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btDelete;
+    }
+}
+
---
+++ TelephoneBook/Form1.cs
@@ -0,0 +1,96 @@
+﻿
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelephoneBook_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Database=telephonebook;Uid=root;Pwd=root;");
+
+        #region События
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter($"INSERT INTO `accounts`(`Name`, `Phone`) VALUES ('{tbName.Text}','{tbNumber.Text}')", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            SelectAll();
+        }
+
+        private void tbFind_TextChanged(object sender, EventArgs e)
+        {
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter($"select * from accounts where name like '%{tbFind.Text}%' or phone like '%{tbFind.Text}%'", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+                dgvFind.DataSource = dataTable;
+            }
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            int indexRow = dgvFind.CurrentCell.RowIndex;
+            //string text = indexRow.ToString();
+
+            string id = dgvFind.Rows[indexRow].Cells["id"].Value.ToString();
+            //MessageBox.Show(text);
+            //MessageBox.Show(id);
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter($"DELETE FROM `accounts` WHERE id = {id}", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+            }
+            MessageBox.Show($"Контакт под номером: {id} удален", "Удаление");
+
+            // Обновление таблиц
+            tbFind_TextChanged(sender, e);
+            SelectAll();
+        }
+        #endregion
+
+        #region Вспомогательные методы
+        /// <summary>
+        /// Показать все данные с таблицы БД accounts
+        /// </summary>
+        private void SelectAll()
+        {
+            using (conn)
+            {
+                MySqlDataAdapter SDA = new MySqlDataAdapter($"select * from accounts", conn);
+                DataTable dataTable = new DataTable();
+                SDA.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        #endregion
+
+
+    }
+
+
+}
